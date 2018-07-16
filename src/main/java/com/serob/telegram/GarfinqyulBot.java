167f130d858b86266d3eb16e49fb9d2fe19ff3eb
Serob Balyan <serob.balyan@gmail.com>
2018-07-17 02:30:10 +0300

    Initial commit

---
+++ src/main/java/com/serob/telegram/GarfinqyulBot.java
@@ -0,0 +1,50 @@
+package com.serob.telegram;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by Serob on 17.07.2018.
+ */
+public class GarfinqyulBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        if (!update.getMessage().hasText())
+            return;
+
+        String messageText = update.getMessage().getText();
+        String firstName = update.getMessage().getFrom().getFirstName();
+
+        SendMessage response = new SendMessage();
+        response.setChatId(update.getMessage().getChatId());
+
+        switch (messageText){
+            case "/start" :
+            case "/help" :
+                response.setText("Please, enter Your name)))");
+                break;
+            default :
+                if(firstName.equalsIgnoreCase(messageText.trim())) {
+                    response.setText("Hello, annnasun " + messageText);
+                } else {
+                    response.setText("I know that your name isn't " + messageText + ", annsun!");
+                }
+        }
+
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBotUsername() {
+        return "Garfinqyul bot";
+    }
+
+    public String getBotToken() {
+        return "663131521:AAGICw6AQiaxYv_AHOiTLu5ov5lqgns8Big";
+    }
+}
